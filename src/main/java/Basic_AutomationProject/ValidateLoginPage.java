c34d463923df41ac41fe599c1f99a15b7004889b
DeepakHaraleQA94 <haraledeepak6@gmail.com>
2026-04-20 09:30:22 +0530

    new code

---
+++ src/main/java/Basic_AutomationProject/ValidateLoginPage.java
@@ -0,0 +1,36 @@
+package Basic_AutomationProject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class ValidateLoginPage {
+
+	@Test	
+	public void verifyLoginPage() {
+		
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		
+		 String homeURL = driver.getCurrentUrl();
+		 System.out.println(homeURL);
+		 
+		 if(homeURL.equals("https://www.saucedemo.com/inventory.html")) {
+			 System.out.println("TC Passed");
+		 }else {
+			 
+			 System.out.println("TC failed");
+		 }
+		
+		driver.close();
+		}
+
+		
+	}
+
